22fef2fd9c91c46113e3335c4da23f4305654545
Denis Plzak <plzak.denis@gmail.com>
2019-09-24 14:52:24 +0200

    Added axios to the projects, async/await request to unsplashed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 
 class App extends React.Component {
 
-  onSearchSubmit(term) {
-    console.log(term);
+  async onSearchSubmit(term) {
+   const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID 2bc2106866f21d4a7bb57e53acb948355972a9e82f6b593624d26d6f8f2c4585"
+      }
+    });
+    console.log(response.data.results);
   }
 
   render () {
